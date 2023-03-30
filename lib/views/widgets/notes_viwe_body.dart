42fe04a33a1366705e35afc98189d44fed1b528d
Kerollos Boles <boleskoko9@gmail.com>
2023-03-30 20:10:19 +0200

    add column

--- lib/views/widgets/notes_viwe_body.dart
+++ lib/views/widgets/notes_viwe_body.dart
@@ -1,7 +1,13 @@
+import 'package:flutter/material.dart';
+
 class NoteViewBody extends StatelessWidget{
   const NoteViewBody({Key? key}) : super(key: key)
   @override
   Widget build(BuildContext context)
-   return container();
+   return Column(
+    children: [
+          CustomAppBar()
+    ],
+   ); //Colum 
   }
 }
