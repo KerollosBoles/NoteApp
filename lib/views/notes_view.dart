23ec794b47c0334ec3dfb7cb43295e2b7c8dfa00
Kerollos Boles <boleskoko9@gmail.com>
2023-03-30 20:01:18 +0200

    the body of the note app

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
 class Notesview extends StatelessWidget{
   const Notesview({Key? key}) : super(key: key)
   @override
   Widget build(BuildContext context){ 
    return Scaffold(
-    appBar: ,
+    body:  ,
    );
   }
-
-
-
+}
+class NoteViewBody extends StatelessWidget{
+  const NoteViewBody({Key? key}) : super(key: key)
+  @override
+  Widget build(BuildContext context){ 
+   return Scaffold(
+    body:  ,
+   );
+  }
 }
