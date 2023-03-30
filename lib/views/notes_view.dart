52ef95f1b03b03e381e655c71c4fd69ab39a336b
Kerollos Boles <boleskoko9@gmail.com>
2023-03-30 10:51:52 +0200

    add retrun value to notes view folder and return scaffold

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Notesview extends StatelessWidget{
+  const Notesview({Key? key}) : super(key: key)
+  @override
+  Widget build(BuildContext context){ 
+   return Scaffold();
+  }
+
+
+
+}
