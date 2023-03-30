a90e371aafe41fc445c4de3a02781d382638cb8a
Kerollos Boles <boleskoko9@gmail.com>
2023-03-30 20:15:36 +0200

    make a the raw of your widget

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -1,14 +1,11 @@
-import 'dart:ffi';
 import 'package:flutter/material.dart';
 
 class NoteViewBody extends StatelessWidget{
   const NoteViewBody({Key? key}) : super(key: key)
   @override
   Widget build(BuildContext context)
-   return Column(
-    children: [
-          CustomAppBar()
-    ],
-   ); //Colum 
-  }
-}
+   return Row(children: [
+
+
+   ],);
+  )
