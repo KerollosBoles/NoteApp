8241dd31af84af07d54e464851d3535fb5867717
Kerollos Boles <boleskoko9@gmail.com>
2023-03-31 20:57:09 +0200

    add the padding and edge Inserts

--- lib/views/widgets/notes_viwe_body.dart
+++ lib/views/widgets/notes_viwe_body.dart
@@ -5,13 +5,16 @@ class NoteViewBody extends StatelessWidget{
   const NoteViewBody({Key? key}) : super(key: key)
   @override
   Widget build(BuildContext context)
-   return Column(
-    children: const [
-      SizedBox(
-        height:50,
-      )
-          CustomAppBar(),
-    ],
-   ); //Colum 
-  }
-}
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: const [
+            SizedBox(
+              height:50,
+            )
+                CustomAppBar(),
+          ],
+          ),
+        ); //Colum 
+        }
+      }
