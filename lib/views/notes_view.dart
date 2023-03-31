4e00c13f885a74e180001e5a0fdd765ba57fd1ef
Kerollos Boles <boleskoko9@gmail.com>
2023-03-31 21:11:22 +0200

    making a note item

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -2,12 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:notes_app/views/widgets/notes_ciew_body.dart';
-class NoteView extends StatelessWidget{
-  const NoteView({Key? key}) : super(key: key)
+
+class NoteView extends StatelessWidget {
+  const NoteView({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context){ 
-   return Scaffold(
-    body:NoteViewBody,
-   );
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NoteViewBody,
+    );
+  }
+}
+
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      return Container();
+    );
   }
 }
