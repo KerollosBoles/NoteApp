fe388c125893dd974117246e0d37d29ec83cd22c
Kerollos Boles <boleskoko9@gmail.com>
2023-03-31 20:58:22 +0200

    changing the horizontal value of the EdgeInserts

--- lib/views/widgets/notes_viwe_body.dart
+++ lib/views/widgets/notes_viwe_body.dart
@@ -6,7 +6,7 @@ class NoteViewBody extends StatelessWidget{
   @override
   Widget build(BuildContext context)
   return Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 16),
+    padding: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
           children: const [
             SizedBox(
