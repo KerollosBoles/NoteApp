dd1bb5974e716f44f225830fb6e81f5637ada997
Kerollos Boles <boleskoko9@gmail.com>
2023-03-31 21:07:54 +0200

    importing a new package to the notes view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-
+import 'package:notes_app/views/widgets/notes_ciew_body.dart';
 class NoteView extends StatelessWidget{
   const NoteView({Key? key}) : super(key: key)
   @override
