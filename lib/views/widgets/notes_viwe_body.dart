77dc459f4c0a5b30ca50cb40eb62594c4481e917
Kerollos Boles <boleskoko9@gmail.com>
2023-04-12 17:19:24 +0200

    create a NoteList view for looping the note item

--- lib/views/widgets/notes_viwe_body.dart
+++ lib/views/widgets/notes_viwe_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notesapp/views/widgets/custom_app_bar.dart';
-
+import 'package:notesapp/views/widget/notes_item.dart';
 class NoteViewBody extends StatelessWidget{
   const NoteViewBody({Key? key}) : super(key: key)
   @override
@@ -13,9 +13,22 @@ class NoteViewBody extends StatelessWidget{
               height:50,
             ),
                 CustomAppBar(),
-                
+                NoteViewBody(),
           ],
           ),
         ); //Colum 
         }
       }
+      class NotesListView extends StatelessWidget {
+  const NotesListView({Key? key}):super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+      return ListView.builder(itemBuilder: (context, index){
+        return const NotesItem();
+    })
+  
+  
+  
+  }
+}
