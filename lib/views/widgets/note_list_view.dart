4db9e8161a64e0ff22c4f8507261d509e8cfed5f
Kerollos Boles <boleskoko9@gmail.com>
2023-04-14 01:14:52 +0200

    changing the horizontal value

--- lib/views/widgets/note_list_view.dart
+++ lib/views/widgets/note_list_view.dart
@@ -7,7 +7,7 @@ class NotesListView extends StatelessWidget {
   Widget build(BuildContext context) {
       return ListView.builder(itemBuilder: (context, index){
         return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
+          padding: const EdgeInsets.symmetric(vertical: 4),
           child: const NoteItem(),
       );
     );
