7841037206583be749f6433369f14e294cdcd3d9
Kerollos Boles <boleskoko9@gmail.com>
2023-04-14 23:36:28 +0200

    put the final touch of our space for the 'Add' bottom

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -23,6 +23,9 @@ class AddNoteBottomSheet extends StatelessWidget {
               hint: 'Content',
               maxLines: 5,
             ),
+             SizedBox(
+              height: 32,
+            ),
             CustomBottom(),
             SizedBox(
               height: 16,
@@ -34,31 +37,3 @@ class AddNoteBottomSheet extends StatelessWidget {
   }
 }
 
-class CustomBottom extends StatelessWidget {
-  const CustomBottom({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-        color: kPrimaryColor,
-        borderRadius: BorderRadius.circular(
-          8,
-      )
-      ),
-      child: const Center(
-        child: Text(
-          'Add',
-          color: TextStyle(
-            color: Colors.black,
-            fontStyle: 20,
-            fontWeight: FontWeight.bold,
-          )
-          
-          ),
-      )
-    );
-  }
-}
