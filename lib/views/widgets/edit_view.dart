804a685ccdc808d495eb233528e249e10d1c7def
Kerollos Boles <boleskoko9@gmail.com>
2023-04-15 02:51:13 +0200

    making a new file for editing note

---
+++ lib/views/widgets/edit_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class EditNote extends StatelessWidget {
+  const EditNote({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
